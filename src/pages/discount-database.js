bd93401e90c68c343887283f8464c8aab51c29e7
eric-craft <ecfleet12@gmail.com>
2025-11-25 20:11:14 -0500

    initial google maps commit

--- src/pages/discount-database.js
+++ src/pages/discount-database.js
@@ -1,8 +1,26 @@
+import GoogleMapReact from 'google-map-react';
 export default function Overview() {
+  const defaultProps = {
+    center: {
+      lat: 10.99835602,
+      lng: 77.01502627
+    },
+    zoom: 11
+  };
+
   return (
     <div className="p-8 text-center">
       <h1 className="text-3xl font-bold mb-4">Discount Database</h1>
       <p className="text-gray-600">Welcome to the Discount Database.</p>
+      <div style={{ height: '100vh', width: '80%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "" }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+        yesIWantToUseGoogleMapApiInternals
+      >
+      </GoogleMapReact>
+    </div>
     </div>
   );
 }
